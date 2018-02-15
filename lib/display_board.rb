5e0d402264d2e72e3658408d97ee4e33d463288b
Tyson Cadorette <tyson.cadorette@wework.com>
2018-02-15 21:15:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "   " + "|" + "   " + "|"+ "   "
+  puts "A Tic Tac Toe Board"
+  print "   |   |   \n"
   puts "-----------"
-  puts "   " + "|" + "   " + "|"+ "   "
+  print "   |   |   \n"
   puts "-----------"
-  puts "   " + "|" + "   " + "|"+ "   "
+  print "   |   |   \n"
 end
